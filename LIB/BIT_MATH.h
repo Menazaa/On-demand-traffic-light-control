a22b6eb2a5bf325fc1e3ff31f638ed7513facdbd
Mohamed Ahmed <myyli1999@gmail.com>
2022-12-12 19:48:52 +0200

    adding lib files

---
+++ LIB/BIT_MATH.h
@@ -0,0 +1,13 @@
+/*
+ * BIT_MATH.h
+ *
+ *  Created on: Dec 12, 2022
+ *      Author: moham
+ */
+
+#ifndef LIB_BIT_MATH_H_
+#define LIB_BIT_MATH_H_
+
+
+
+#endif /* LIB_BIT_MATH_H_ */
---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,13 @@
+/*
+ * STD_TYPES.h
+ *
+ *  Created on: Dec 12, 2022
+ *      Author: moham
+ */
+
+#ifndef LIB_STD_TYPES_H_
+#define LIB_STD_TYPES_H_
+
+
+
+#endif /* LIB_STD_TYPES_H_ */
