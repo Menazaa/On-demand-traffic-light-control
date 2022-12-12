22d2fddb7a7c229a752534c90d47f9a06197d508
Mohamed Ahmed <myyli1999@gmail.com>
2022-12-12 20:37:15 +0200

    add bit math macro functions

--- LIB/BIT_MATH.h
+++ LIB/BIT_MATH.h
@@ -9,5 +9,16 @@
 #define LIB_BIT_MATH_H_
 
 
+// 4 NORMAL MACROS [NORMAL REPLACEMENT]
+#define SET_BIT(VAR,BIT)    (VAR |=(0x01<<BIT))//BRACKETS BEC I CAN USE IT INSIDE STH. (OPERATION ARRANGEMENTS) // TEXT PLACEMENT TIS IS MICRO
+#define CLR_BIT(VAR,BIT)    (VAR &= (~(0x01<<BIT)))
+#define GET_BIT(VAR,BIT)    ((VAR>>BIT) & 0x01)
+#define TOG_BIT(VAR,BIT)    (VAR ^=(0x01<<BIT))
+/*
+ * MACRO LIKE FUNCTION ?? Can't get it search
+ * #define FUNC(ARG1,ARG2)  (ARG1+ARG2)
+ * 0b00001111
+ * SetPinVal(7,GET_BIT(0b00001111,7))
+*/
 
 #endif /* LIB_BIT_MATH_H_ */
