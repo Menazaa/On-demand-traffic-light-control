cb12a986bdad740ac51a83cf7120eb9bfe8f38b8
Mohamed Ahmed <myyli1999@gmail.com>
2022-12-13 00:36:47 +0200

    adding Port Type definition

--- MCAL/DIO/DIO_Private.h
+++ MCAL/DIO/DIO_Private.h
@@ -8,6 +8,21 @@
 #ifndef MCAL_DIO_DIO_PRIVATE_H_
 #define MCAL_DIO_DIO_PRIVATE_H_
 
+
+// PORT DATA TYPE
+typedef struct{
+	u8	PIN;	// PIN REGISTER
+	u8	DDR;	// DATA DIRECTION REGISTER
+	u8	PORT;	// PORT REGISTER
+}DIO_t;
+
+// DIO PORTS REGISTERS DEFINATION
+
+volatile DIO_t*   const PORTA       = (DIO_t*)0x39;
+volatile DIO_t*   const PORTB       = (DIO_t*)0x36;
+volatile DIO_t*   const PORTC       = (DIO_t*)0x33;
+volatile DIO_t*   const PORTD       = (DIO_t*)0x30;
+
 // PORTx Definitions
 typedef enum{
 	PORTA,
