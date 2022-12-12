4935d9bd4b1e07c98cf4b3893f3e6a8ab2a14867
Mohamed Ahmed <myyli1999@gmail.com>
2022-12-12 19:46:57 +0200

    editing DIO driver file structure

---
+++ MCAL/DIO/DIO_Interface.h
@@ -0,0 +1,13 @@
+/*
+ * DIO.h
+ *
+ *  Created on: Dec 12, 2022
+ *      Author: Mohamed Menazaa
+ */
+
+#ifndef MCAL_DIO_DIO_INTERFACE_H_
+#define MCAL_DIO_DIO_INTERFACE_H_
+
+
+
+#endif /* MCAL_DIO_DIO_INTERFACE_H_ */
---
+++ MCAL/DIO/DIO_Private.h
@@ -0,0 +1,13 @@
+/*
+ * DIO_Private.h
+ *
+ *  Created on: Dec 12, 2022
+ *      Author: moham
+ */
+
+#ifndef MCAL_DIO_DIO_PRIVATE_H_
+#define MCAL_DIO_DIO_PRIVATE_H_
+
+
+
+#endif /* MCAL_DIO_DIO_PRIVATE_H_ */
